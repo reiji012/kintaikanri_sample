f1c0dc1c06dbec30c58e02480f94e64f7b7a09c4
ReijiMatsumura <p.matsumura@ailes-dor.co.jp>
2018-10-18 16:51:49 +0900

    style修正

--- app/javascript/packs/components/button.vue
+++ app/javascript/packs/components/button.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="switchButton" style="display: flex;">
-        <div class="box" >
+        <div class="box" style="border-radius: 20px;">
             <router-link to="/">帰社登録</router-link>
         </div>
-        <div class="box" >
+        <div class="box" style="border-radius: 20px;">
             <router-link to="recordCheck">記録確認</router-link>
         </div>
     </div>
